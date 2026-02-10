afb90ea2776cf9ccb092834aa9abc9818eae2e92
MatheusDoRosario <matheusxdargws@gmail.com>
2026-02-10 13:24:50 -0300

    Adiciona todos os métodos da SerieService

--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -1,11 +1,13 @@
 package br.com.alura.screenmatch.service;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SerieService {
@@ -13,12 +15,32 @@ public class SerieService {
 
     @Autowired
     private SerieRepository repository;
-
     public List<SerieDTO> obterTodasAsSeries() {
-        return repository.findAll()
-                .stream()
+        return converteDados(repository.findAll());
+
+    }
+
+    public List<SerieDTO> obterTop5Series() {
+        return converteDados(repository.findTop5ByOrderByAvaliacaoDesc());
+
+    }
+
+    public List<SerieDTO> obterLancamentos(){
+        return converteDados(repository.encontrarEpisodiosMaisRecentes());
+    }
+
+    private List<SerieDTO> converteDados(List<Serie> series) {
+        return series.stream()
                 .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
                 .toList();
     }
 
+    public SerieDTO obterPorId(Long id) {
+        Optional<Serie> serie = repository.findById(id);
+        if (serie.isPresent()) {
+            Serie s = serie.get();
+            return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse());
+        }
+        return null;
+    }
 }
